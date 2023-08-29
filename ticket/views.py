82b2be3ae6a54a4b47063b034028c27b2af54ef9
Ay-iman <ayomiderabiu00@gmail.com>
2023-08-29 11:37:59 +0100

    alx projects ongoing

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Ticket
+from .form import CreateTicketForm, UpdateTicketForm
 
-# Create your views here.
+"""For Coustomers"""
+
+# creating a ticket
+def create_ticket(request):
+    if request.method == 'POST':
+        form = CreateTicketForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.created_by = request.user
+            var.ticket_status = 'Pending'
+            var.save()
+            messages.info(request, 'Your ticket has been successfully submitted. An engineer would be assigned soon')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Something went wrong. Please check form input')
+            return redirect('create-ticket')
+    else:
+        form = CreateTicketForm()
+        context - {'form':form}
+        return render(request, 'ticket/create_ticket.html', context)
+    
+
+#updating a ticket
+def update_ticket(request, pk):
+    ticket = Ticket.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UpdateTicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            form.save
+            messages.info(request, 'Your ticket info has been updated and all the changes are saved in the database')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Something went wrong. Please check form input')
+            # return redirect('create-ticket')
+    else:
+        form = UpdateTicketForm(instance=ticket)
+        context - {'form':form}
+        return render(request, 'ticket/update_ticket.html', context)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from .form import RegisterCustomerForm
 
-# Create your views here.
+# register a customer
+def register_customer(request):
+    if request.method == 'POST':
+        form = RegisterCustomerForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_customer = True
+            var.save()
+            messages.info(request, 'Your account has been successfully registered. Please login to continue')
+            return redirect('login')
+        else:
+            messages.warning(request, 'something went wrong, Please check form inputs')
+            return redirect('register-customer')
+    else:
+        form = RegisterCustomerForm()
+        context = {'form':form}
+        return render(request, 'user/register_customer.html', context)
+    
+
+# login a user
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            messages.info(request, 'Login succesfull. Please enjoy your session')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'something went wrong. please check form input')
+            return redirect('login')
+        
+    else:
+        return render(request, 'users/login.html')
+    
+# logout a user
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Your session has ended. Please log in to continue')
+    return redirect('login')
